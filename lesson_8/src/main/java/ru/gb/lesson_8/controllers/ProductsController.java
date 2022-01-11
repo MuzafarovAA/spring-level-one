531ef0218b3a2b925986831d7ddf99157b7a0c50
artmuz42 <artmuz@mail.ru>
2022-01-11 21:38:10 +0500

    Lesson 8 Homework

--- lesson_8/src/main/java/ru/gb/lesson_8/controllers/ProductsController.java
+++ lesson_8/src/main/java/ru/gb/lesson_8/controllers/ProductsController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RequestMapping("/api/products")
 @RestController
-public class ProductsRestController {
+public class ProductsController {
     private ProductsService productsService;
 
     @Autowired
